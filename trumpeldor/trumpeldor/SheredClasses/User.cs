8dc667bc0c8ad106e4bc1661afd225b204fab83f
or <or512749@gmail.com>
2018-11-19 17:01:06 +0200

    server connection on application side

---
+++ trumpeldor/trumpeldor/SheredClasses/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trumpeldor.SheredClasses
+{
+    class User
+    {
+        public String Email { get; set; }
+        public List<String> PlayersAges { get; set; }
+        public String Name { get; set; }
+    }
+}
