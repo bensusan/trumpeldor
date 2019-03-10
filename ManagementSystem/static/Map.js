cfd72cb723309c9a7eab00f68996becd3bad0809
yonisGit <yoniel@post.bgu.ac.il>
2019-03-10 22:04:38 +0200

    chn

--- ManagementSystem/static/Map.js
+++ ManagementSystem/static/Map.js
@@ -2,6 +2,12 @@
 let curPosClicked;
 let curMarker;
 let coordinates_of_last_click;
+let points= [
+    {lat: 31.263465932844372, lng: 34.80194628238678},
+    {lat: 31.3114324, lng: 34.5555555555555},
+    {lat: 31.45255555372, lng: 34.9238678},
+    {lat: 31.262773527283052, lng: 34.802075028419495}
+];
 
 function initMapAndAttractions(){
     initMap();
@@ -13,7 +19,24 @@ function initMap() {
         center: {lat: 31.262860, lng: 34.801753}
     });
     listenerForMap(map);
+    initPoints();
 }
+
+  function initPoints(){
+  for (var i = 0; i < points.length; i++) {
+    addPoint2(points[i])
+  }
+  }
+
+  function addPoint2(p){
+    var myLatLng = {lat: p.lat, lng: p.lng};
+    var m = new google.maps.Marker({
+      position:myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+    m.setMap(map);
+  }
 
 function listenerForMap(map){
     google.maps.event.addListener(map, 'click', (function(event) {
@@ -23,6 +46,11 @@ function listenerForMap(map){
         }
         curPosClicked = positionInMap(event.latLng.lat(), event.latLng.lng());
         curMarker = markAttraction(curPosClicked);
+        editBTN=document.getElementById('edit_attraction');
+        editBTN.addEventListener('click', function(event) {
+        if(points.indexOf(curPosClicked)!=-1)
+            {}
+        });
         addBTN = document.getElementById('add_attraction');
         addBTN.addEventListener('click', function(event) {
         localStorage.setItem("addedPoint", coordinates_of_last_click);
