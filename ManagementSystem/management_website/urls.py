7252e4c323f3a109756136bc40feae04e408b4cb
yonisGit <yoniel@post.bgu.ac.il>
2019-03-10 16:52:42 +0200

    paths

--- ManagementSystem/management_website/urls.py
+++ ManagementSystem/management_website/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     path('attractions/', views.manage_attractions_page, name='attractions'),
     path('add_attraction/', views.add_attraction_page, name='add_attraction'),
     path('main/', views.main_page),
-
+    # path('short_path/', views.short_path),
+    # path('medium_path/', views.medium_path),
+    # path('long_path/', views.long_path),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
