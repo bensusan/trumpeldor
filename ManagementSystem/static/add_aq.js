48ed5fcf9264acb5f212f553cc3d136f0e733efc
yonisGit <yoniel@post.bgu.ac.il>
2019-03-17 22:25:54 +0200

    sa

--- ManagementSystem/static/add_aq.js
+++ ManagementSystem/static/add_aq.js
@@ -31,10 +31,8 @@ function addAQ(){
     function funcToGetAttraction(attractionsJSON) {
         let name = localStorage.getItem("name_for_add_aq");
         let desc = localStorage.getItem("desc_for_add_aq");
-        alert("aaaaaaaaaa");
       // alert("in get name! "+"of the origin : " + lat + " , " + lng);
-      attractionsJSON.forEach(function (attr) {
-          alert("the id is: "+attr['id']);
+        attractionsJSON.forEach(function (attr) {
         let p = {name: attr['name'], description:attr['description']};
          // alert("in get name! "+"of the origin : " + name + " , " + desc + "\n of the other: "+p.name +" , "+ p.description);
         if(p.name==name && p.description==desc)
@@ -44,12 +42,14 @@ function addAQ(){
             let ans3=document.getElementById("ans3").value;
             let ans4=document.getElementById("ans4").value;
             let answers = [ans1,ans2,ans3,ans4]; // might need to be a list(if theres a way) and not an array
+
             let american_question_to_send = {
                     question :document.getElementById("ques").value
                     ,answers: answers
                     ,indexOfCorrectAnswer: Number(document.getElementById("correctAns").value)
                     ,attraction:attr['id'] //atraction id needs to be here
             };
+
             postRequestAmericanQuestion(american_question_to_send,attr['id']);
         }
       });
--- ManagementSystem/static/attractions.js
+++ ManagementSystem/static/attractions.js
@@ -46,6 +46,7 @@ function getRequestAttractions(funcOnAttractions){
     // serverRequest("GET", funcOnAttractions, 'http://192.168.1.12:12344/managementsystem/attraction/?format=json');
     // the server port and my ip
     serverRequest("GET", funcOnAttractions, 'http://10.0.0.7:12344/managementsystem/attraction/?format=json');
+    alert("need to remove this alert and fix funcToGetAttraction()!")
 }
 
 function initAttractionsMarkers() {
