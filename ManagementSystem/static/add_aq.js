e8c1b4caa7765358e2b7ef2d70b19b36c0a1e19b
yonisGit <yoniel@post.bgu.ac.il>
2019-03-18 15:20:37 +0200

    adding aq works!

--- ManagementSystem/static/add_aq.js
+++ ManagementSystem/static/add_aq.js
@@ -1,16 +1,16 @@
 window.onload = function() {
-    alert("hi");
+    // alert("way");
     var addAqBTN = document.getElementById('finish_add_aq');
     addAqBTN.addEventListener('click', function() {
         getRequestAttractions(funcToGetAttraction);
         //alert("2");
         // window.location.href='/add_hint';
     });
-
-    var moveonBTN = document.getElementById('move_on');
-    moveonBTN.addEventListener('click', function() {
-        window.location.href='/add_hint';
-    });
+    //
+    // var moveonBTN = document.getElementById('move_on');
+    // moveonBTN.addEventListener('click', function() {
+    //     window.location.href='/add_hint';
+    // });
 
 }
 
@@ -29,7 +29,7 @@ function postRequestAmericanQuestion(aq,attr_id){
       // alert("in get name! "+"of the origin : " + lat + " , " + lng);
         attractionsJSON.forEach(function (attr) {
         let p = {name: attr['name'], description:attr['description']};
-        alert("in get name! "+"of the origin : " + name + " , " + desc + "\n of the other: "+p.name +" , "+ p.description);
+       // alert("in get name! "+"of the origin : " + name + " , " + desc + "\n of the other: "+p.name +" , "+ p.description);
         if(p.name===name && p.description===desc)
         {
             let ans1=document.getElementById("ans1").value;
@@ -45,33 +45,11 @@ function postRequestAmericanQuestion(aq,attr_id){
                     ,attraction:attr['id'] //atraction id needs to be here
             };
             postRequestAmericanQuestion(american_question_to_send,attr['id']);
+            window.location.href='/add_hint';
         }
       });
         // alert("cant believe this is happenning!");
     }
 
-
-function addAQ(){
-
-    // let ans1=document.getElementById("ans1").value;
-    // let ans2=document.getElementById("ans2").value;
-    // let ans3=document.getElementById("ans3").value;
-    // let ans4=document.getElementById("ans4").value;
-    // let answers = [ans1,ans2,ans3,ans4]; // might need to be a list(if theres a way) and not an array
-    //
-    //
-    // let american_question_to_send = {
-    //         question :document.getElementById("ques").value
-    //         ,answers: answers
-    //         ,indexOfCorrectAnswer: Number(document.getElementById("correctAns").value),
-    //         attraction:"" //atraction id needs to be here
-    // };
-        // postRequestAQ(american_question_to_send);
-    //alert("1");
-        getRequestAttractions(funcToGetAttraction);
-      //  alert("2");
-        // postRequestAmericanQuestion(american_question_to_send);
-        // window.location.href='/add_hint';
-}
 
 
