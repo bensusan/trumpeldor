f19a65aa665068b246cae6c40959103403850cb3
yonisGit <yoniel@post.bgu.ac.il>
2019-03-18 18:25:58 +0200

    chngs

--- ManagementSystem/static/add_attraction.js
+++ ManagementSystem/static/add_attraction.js
@@ -6,37 +6,40 @@ var loadFile = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
 
-window.onload=function(){
-(function localFileVideoPlayer() {
-	'use strict'
-  var URL = window.URL || window.webkitURL
-  var displayMessage = function (message, isError) {
-    var element = document.querySelector('#message')
-    element.innerHTML = message
+
+function localFileVideoPlayer() {
+	'use strict';
+  let URL = window.URL || window.webkitURL;
+  let displayMessage = function (message, isError) {
+    let element = document.querySelector('#message');
+    element.innerHTML = message;
     element.className = isError ? 'error' : 'info'
-  }
-  var playSelectedFile = function (event) {
-    var file = this.files[0]
-    var type = file.type
-    var videoNode = document.querySelector('video')
-    var canPlay = videoNode.canPlayType(type)
-    if (canPlay === '') canPlay = 'no'
-    var message = 'Can play type "' + type + '": ' + canPlay
-    var isError = canPlay === 'no'
+  };
+  let playSelectedFile = function (event) {
+    let file = this.files[0];
+    let type = file.type;
+    let videoNode = document.querySelector('video');
+    let canPlay = videoNode.canPlayType(type);
+    if (canPlay === '') canPlay = 'no';
+    let message = 'Can play type "' + type + '": ' + canPlay;
+    let isError = canPlay === 'no';
     //displayMessage(message, isError)
 
     if (isError) {
       return
     }
 
-    var fileURL = URL.createObjectURL(file)
-    videoNode.src = fileURL
+    let fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
 
-  }
-  var inputNode = document.querySelector('input')
-  inputNode.addEventListener('change', playSelectedFile, false)
-})()
+  };
+  let inputNode = document.querySelector('input');
+  inputNode.addEventListener('change', playSelectedFile, false);
+}
 
+
+window.onload=function(){
+    localFileVideoPlayer();
     }
 
     function showDataCollected() {
--- ManagementSystem/static/add_hint.js
+++ ManagementSystem/static/add_hint.js
@@ -6,23 +6,59 @@ var loadFile = function(event) {
 window.onload = function () {
 var textHintBTN = document.getElementById('add_text_hint');
         textHintBTN.addEventListener('click', function() {
-            alert("yo");
             var textLine = document.getElementById("text_hint_id");
             textLine.style.display = "inline";
         });
 
 var picHintBTN = document.getElementById('add_pic_hint');
         picHintBTN.addEventListener('click', function() {
-
+            var clickHere = document.getElementById("click_here_label");
+            clickHere.style.display = "inline";
+            var outpic = document.getElementById("output");
+            outpic.style.display = "inline";
         });
 
 var vidHintBTN = document.getElementById('add_vid_hint');
         vidHintBTN.addEventListener('click', function() {
-
+            var thevid = document.getElementById("vid_hint_id");
+            thevid.style.display = "inline";
+            var thevidbrowse = document.getElementById("vid_hint_browse_id");
+            thevidbrowse.style.display = "inline";
         });
+
+        localFileVideoPlayer();
 
 };
 
+function localFileVideoPlayer() {
+	'use strict';
+  let URL = window.URL || window.webkitURL;
+  let displayMessage = function (message, isError) {
+    let element = document.querySelector('#message');
+    element.innerHTML = message;
+    element.className = isError ? 'error' : 'info'
+  };
+  let playSelectedFile = function (event) {
+    let file = this.files[0];
+    let type = file.type;
+    let videoNode = document.querySelector('video');
+    let canPlay = videoNode.canPlayType(type);
+    if (canPlay === '') canPlay = 'no';
+    let message = 'Can play type "' + type + '": ' + canPlay;
+    let isError = canPlay === 'no';
+    //displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    let fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
+
+  };
+  let inputNode = document.querySelector('input');
+  inputNode.addEventListener('change', playSelectedFile, false);
+}
 
 
 function finishHint() {
