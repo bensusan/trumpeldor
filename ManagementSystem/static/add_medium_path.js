8986f075e6d330128b206ecf1ace129618058536
yonisGit <yoniel@post.bgu.ac.il>
2019-03-20 21:38:05 +0200

    ewq

--- ManagementSystem/static/add_medium_path.js
+++ ManagementSystem/static/add_medium_path.js
@@ -1,7 +1,36 @@
 let short_path_points_for_medium = JSON.parse(localStorage.getItem("the_points_of_the_short_path"));
+let short_path_points_for_medium_lat = [];
+let short_path_points_for_medium_lng = [];
+
 window.onload=function () {
+    let i;
+    for(i=0;i<short_path_points_for_medium.length;i++)
+    {
+        short_path_points_for_medium_lat.push(short_path_points_for_medium[i].lat);
+        short_path_points_for_medium_lng.push(short_path_points_for_medium[i].lng);
+    }
+
     getRequestAttractions(markAttractionsOfMediumPath);
 };
+
+
+
+function initMapAndAttractions(){
+    initMap();
+    initAttractionsMarkersOfMediumPath();
+}
+
+function initMap() {
+     map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: {lat: 31.262860, lng: 34.801753}
+    });
+    //initAttractionsMarkersOfShortPath();
+    listenerForMap();
+   // initPoints();
+    document.getElementById("things_to_fix").innerHTML = "need to fix: if we click on to points and then on the 'add' button it adds both of them." ;
+
+}
 
 function getRequestMediumPath(funcOnMedPath){
     // the server port and my ip
@@ -17,12 +46,16 @@ function postRequestMediumPath(medium_path){
 
 function markAttractionsOfMediumPath(attractionsJSON){
     attractionsJSON.forEach(function (attr) {
-        let pos = {lat: attr['x'], lng: attr['y']};
-        if(true) {
-            alert("if point in:"+short_path_points_for_medium.includes(pos) +"\nthe point attr: "+pos+"\n the arr: "+short_path_points_for_medium);
 
-            // localStorage.setItem("title" + pos, "attraction ID: " + attr['id'] + "\nattraction name: " + attr['name'] + "\nposition: (" + attr['x'] + "," + attr['y'] + ")");
-            // markAttractionOfMediumPath(pos);
+        let pos = {lat: attr['x'], lng: attr['y']};
+        let lats=short_path_points_for_medium_lat.map(function (x){return x.toFixed(8)});
+        let lngs=short_path_points_for_medium_lng.map(function (x){return x.toFixed(8)});
+
+        if(lats.includes((pos.lat).toFixed(8))&& lngs.includes((pos.lng).toFixed(8))) {
+            // alert("if point in:"+lats.includes((pos.lat).toFixed(8)) +lngs.includes((pos.lng).toFixed(8)));
+
+            localStorage.setItem("title" + pos, "attraction ID: " + attr['id'] + "\nattraction name: " + attr['name'] + "\nposition: (" + attr['x'] + "," + attr['y'] + ")");
+            markAttractionOfMediumPath(pos);
         }
     });
 }
