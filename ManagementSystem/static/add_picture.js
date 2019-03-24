35d0ae4e9827436bef0402c79bb2eed006d5b180
yonisGit <yoniel@post.bgu.ac.il>
2019-03-24 12:37:46 +0200

    mew

--- ManagementSystem/static/add_picture.js
+++ ManagementSystem/static/add_picture.js
@@ -1,22 +1,6 @@
 
-var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
-    localStorage.setItem("trying", JSON.stringify(image.toString()));
-	var editBTN = document.getElementById('newone');
-        editBTN.addEventListener('click', function() {
-        	var imo = document.getElementById('newone2');
-        	// var imo3 = document.getElementById('newone3');
-            imo.src=URL.createObjectURL(event.target.files[0]);
-
-            document.getElementById("url_of_pic").innerHTML = URL.createObjectURL(event.target.files[0]);
-            // imo3.src=URL.createObjectURL(event.target.files[0]);
-
-        });
-
-
-};
 var suki;
+
 
 function shit(suk) {
     suki=suk;
@@ -28,12 +12,17 @@ function shit(suk) {
 
 
 function encodeImageFileAsURL(element) {
+    var image = document.getElementById('output');
+	image.src = URL.createObjectURL(element.files[0]);
+
     suki="";
+
   var file = element.files[0];
   var reader = new FileReader();
   reader.onloadend = function() {
    //alert(reader.result)
    shit(reader.result)
   }
+  
   reader.readAsDataURL(file);
 }
