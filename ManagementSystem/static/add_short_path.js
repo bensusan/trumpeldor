a5b3cb79d5f58babe5918d9c236a68bacda0d3dd
yonisGit <yoniel@post.bgu.ac.il>
2019-03-29 00:58:27 +0200

    good style for add short path

--- ManagementSystem/static/add_short_path.js
+++ ManagementSystem/static/add_short_path.js
@@ -47,7 +47,11 @@ function initMap() {
                 str_of_points=str_of_points+m.position+"<br />";
             }
             // alert(str_of_points);
+            var border = document.getElementById("border_of_points");
+            border.style.display = "block";
             document.getElementById("showing_added_points").innerHTML = str_of_points;
+            document.getElementById("showing_added_points").style.fontWeight = 'bold';
+
            // alert("point been added! now its: "+ pointsOfPath.toString());
         });
   });
