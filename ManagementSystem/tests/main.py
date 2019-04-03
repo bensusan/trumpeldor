58fd3425091cb9c230cfa8513a484cd61983c0e0
yonisGit <yoniel@post.bgu.ac.il>
2019-04-03 11:49:19 +0300

    added the tests file

--- ManagementSystem/tests/main.py
+++ ManagementSystem/tests/main.py
@@ -14,15 +14,6 @@ from selenium.webdriver.remote.webelement import WebElement
 
 
 def main():
-    # download_path = 'D:\_Guy\d9anime\downloaded'
-    # download('kyoukai no kanata', [12])
-    # print(find('steins gate'))
-    # print(find('nanatsu no taizai imashime no fukkatsu'))
-    # print(find('yuru camp'))
-    # print(fetch_url('http://httpbin.org/headers').replace('\\n', '\n'))
-    # deku.download_episodes(anime_name='A Place Further Than The Universe', path='.', server=RapidVideo)
-    # deku.download_episodes(anime_name='A Place Further Than The Universe', path='.', server=MyCloud)
-    # get_video_links_by_name('A Place Further Than The Universe')
     opts = Options()
     opts.set_headless()
     browser = Chrome(options=opts)
