605a84cf6b61e10d1e5e7958526fdefdf0a00db4
yonisGit <yoniel@post.bgu.ac.il>
2019-04-07 15:13:34 +0300

    JHKJ

--- ManagementSystem/static/feedback.js
+++ ManagementSystem/static/feedback.js
@@ -5,18 +5,18 @@ window.onload = function() {
     let sendBTN = document.getElementById("send_feedback");
     sendBTN.addEventListener('click', function () {
         let feed_type;
-        if(feedback_type.value = 'opt1')
+        if(feedback_type.value == 'opt1')
             feed_type = 'FT';
         else
             feed_type = 'FR';
 
         let feed_to_send = {question:feedback_question.value , kind: feed_type};
+
         postRequestFeedback(feed_to_send);
 
     });
 
 };
-
 
 
 function postRequestFeedback(fb){
