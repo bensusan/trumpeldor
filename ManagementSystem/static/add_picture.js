6c8f2f160398b902bdb698a847f6fcc47b296651
yonisGit <yoniel@post.bgu.ac.il>
2019-04-10 21:09:55 +0300

    sadadasd

--- ManagementSystem/static/add_picture.js
+++ ManagementSystem/static/add_picture.js
@@ -26,3 +26,36 @@ function encodeImageFileAsURL(element) {
 
   reader.readAsDataURL(file);
 }
+
+function funcToSendGame(attractionsJSON) {
+        let name = localStorage.getItem("name_for_add_aq");
+        let desc = localStorage.getItem("desc_for_add_aq");
+      // alert("in get name! "+"of the origin : " + lat + " , " + lng);
+        attractionsJSON.forEach(function (attr) {
+        let p = {name: attr['name'], description:attr['description']};
+       // alert("in get name! "+"of the origin : " + name + " , " + desc + "\n of the other: "+p.name +" , "+ p.description);
+        if(p.name===name && p.description===desc)
+        {
+
+            if(localStorage.getItem("game_kind")=="sliding") {
+                let the_kind = "sliding_puzzle";
+                let data_send = {piecesURLS: suki, width: 4, height: 4};
+                let attr_id = attr['id'];
+                postRequestGame(data_send,attr_id,the_kind);
+            }
+            window.location.href='/add_game_edit'; ////////////////////////////change to both
+        }
+
+    });
+}
+
+function sendTheGameData() {
+    getRequestAttractions(funcToSendGame);
+}
+
+function postRequestGame(data,attr_id,game_kind){
+    //alert("aq blat");
+    serverRequest("POST", function noop(dummy){}, 'http://'+ip+':12344/managementsystem/attraction/'+
+        attr_id+'/'+game_kind+'/',
+        JSON.stringify(data));
+}
