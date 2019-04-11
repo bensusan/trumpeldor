c2c1a8a0fc5258ccaa1f7b9d5aab6db5b4b9462e
yonisGit <yoniel@post.bgu.ac.il>
2019-04-11 20:04:26 +0300

    video works perfect also

--- ManagementSystem/static/add_attraction.js
+++ ManagementSystem/static/add_attraction.js
@@ -2,6 +2,7 @@
 
 
 var suki;
+var sukiVid;
 
 var loadFile = function(event) {
 	var image = document.getElementById('output');
@@ -57,14 +58,16 @@ window.onload=function(){
         // currPoints.push(addedPoint);
         let lat = addedPoint.lat;
         let picArr=[];
-        picArr.push(suki);
+        let vidArr=[];
+        if(suki!=undefined){picArr.push(suki);}
+        if(sukiVid!=undefined){vidArr.push(sukiVid);}
         let lang = addedPoint.lng;
         let attraction_to_send = {
             name:document.getElementById("attr_name").value
             //,x:31.262860,y:34.801753
             ,x:lat ,y:lang
             ,description:document.getElementById("desc").value
-            ,picturesURLS:picArr ,videosURLS:[]};
+            ,picturesURLS:picArr ,videosURLS:vidArr};
         postRequestAttraction(attraction_to_send);
         localStorage.setItem("name_for_add_aq", attraction_to_send.name);
         localStorage.setItem("desc_for_add_aq", attraction_to_send.description);
@@ -92,6 +95,12 @@ function shit(suk) {
 }
 
 
+function shitVid(suk) {
+    sukiVid=suk;
+    // var tmuna = document.getElementById("sukablat");
+    // tmuna.src = suki;
+}
+
 function encodeImageFileAsURL(element) {
     var image = document.getElementById('output');
     image.style.display="inline";
@@ -105,6 +114,21 @@ function encodeImageFileAsURL(element) {
    //alert(reader.result)
    shit(reader.result)
   }
+
+  reader.readAsDataURL(file);
+}
+
+
+function encodeVideoFileAsURL(element) {
+
+    sukiVid="";
+
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function() {
+   //alert(reader.result)
+   shitVid(reader.result)
+  };
 
   reader.readAsDataURL(file);
 }
