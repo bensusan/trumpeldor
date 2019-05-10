1cd5cea5e1ddf6013ec3f6145e0363f035e3246f
yonisGit <yoniel@post.bgu.ac.il>
2019-05-10 15:13:19 +0300

    viddpic in add_info

--- ManagementSystem/static/add_info.js
+++ ManagementSystem/static/add_info.js
@@ -1,9 +1,52 @@
 window.onload = function () {
     getRequestInfo(func_to_show_info);
+
+	let vid_title = document.getElementById("vid_title");
+    let pic_title = document.getElementById("pic_title");
+    let pic_input = document.getElementById("picInput");
+    let pic_output = document.getElementById("output");
+    let vid_itself = document.getElementById("vid_itself");
+    // let vid_input = document.getElementById("video_input");
+    let vid_uploadBTN = document.getElementById("upvidBTN");
+
+    let want_upvid = document.getElementById("add_vid_info");
+    let want_upic = document.getElementById("add_pic_info");
+
+
+    want_upic.addEventListener('click',function () {
+
+		pic_title.style.display = '';
+		pic_input.style.display = '';
+		pic_output.style.display = '';
+		vid_title.style.display = 'none';
+		vid_itself.style.display = 'none';
+		vid_uploadBTN.style.display = 'none';
+
+	});
+
+    want_upvid.addEventListener('click',function () {
+
+		pic_title.style.display = 'none';
+		pic_input.style.display = 'none';
+		pic_output.style.display = 'none';
+		vid_title.style.display = '';
+		vid_itself.style.display = '';
+		vid_uploadBTN.style.display = '';
+	});
+
+
+    localFileVideoPlayer();
 };
 
 let info_txt;
+var attr_for_editing;
+var suki;
+var sukiVid;
 
+function doVideo(){
+    	let vid_input = document.getElementById('video_input');
+        vid_input.click();
+}
 
 function func_to_show_info(infoJSON) {
     infoJSON.forEach(function (info) {
@@ -16,6 +59,22 @@ function func_to_show_info(infoJSON) {
 function clearText(){
     let txt = document.getElementById('subject');
     txt.value="";
+}
+
+function encodeImageFileAsURL(element) {
+    var image = document.getElementById('output');
+	image.src = URL.createObjectURL(element.files[0]);
+
+    suki="";
+
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function() {
+   //alert(reader.result)
+   shit(reader.result)
+  };
+
+  reader.readAsDataURL(file);
 }
 
 function sendInfo(){
@@ -67,3 +126,35 @@ if($('#email').val() == "") {
 });
 
 });
+
+
+
+function localFileVideoPlayer() {
+	'use strict';
+  let URL = window.URL || window.webkitURL;
+  let displayMessage = function (message, isError) {
+    let element = document.querySelector('#message');
+    element.innerHTML = message;
+    element.className = isError ? 'error' : 'info'
+  };
+  let playSelectedFile = function (event) {
+    let file = this.files[0];
+    let type = file.type;
+    let videoNode = document.querySelector('video');
+    let canPlay = videoNode.canPlayType(type);
+    if (canPlay === '') canPlay = 'no';
+    let message = 'Can play type "' + type + '": ' + canPlay;
+    let isError = canPlay === 'no';
+    //displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    let fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
+
+  };
+  let inputNode = document.querySelector('input');
+  inputNode.addEventListener('change', playSelectedFile, false);
+}
