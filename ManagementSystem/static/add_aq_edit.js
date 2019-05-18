9fd648f80598259fd9f349f6514e053e5cde91e4
yonisGit <yoniel@post.bgu.ac.il>
2019-05-18 16:08:23 +0300

    anerican
    q

--- ManagementSystem/static/add_aq_edit.js
+++ ManagementSystem/static/add_aq_edit.js
@@ -1,87 +1,24 @@
 let numberOfAns;
 let numberOfCorrectAns;
+let arrOfAnswers = [];
 
 window.onload = function() {
-    let noOfAns = document.getElementById('noOfAns');
-    let noOfCorrect = document.getElementById('noOfCorrect');
-    let okBTN = document.getElementById('ok_button_to_prepare');
+
     let addAqBTN = document.getElementById('finish_add_aq_btn');
-    let txt1 = document.getElementById('txt1');
-    let txt2 = document.getElementById('txt2');
-
-    let ques = document.getElementById('ques');
-    let ans1 = document.getElementById('ans1');
-    let ans2 = document.getElementById('ans2');
-    let ans3 = document.getElementById('ans3');
-    let ans4 = document.getElementById('ans4');
-    let ans5 = document.getElementById('ans5');
-    let ans6 = document.getElementById('ans6');
-    let ans7 = document.getElementById('ans7');
-    let ans8 = document.getElementById('ans8');
-    let correctAns = document.getElementById('correctAns');
-    let correctAns2 = document.getElementById('correctAns2');
-    let correctAns3 = document.getElementById('correctAns3');
-
-    okBTN.addEventListener('click',function () {
-    numberOfAns = noOfAns.value;
-    numberOfCorrectAns = noOfCorrect.value;
-
-    okBTN.style.display = "none";
-    txt1.style.display = "none";
-    txt2.style.display = "none";
-    noOfAns.style.display = "none";
-    noOfCorrect.style.display = "none";
-    addAqBTN.style.display = "inline";
-    ques.style.display = "inline";
-    ans1.style.display = "inline";
-    ans2.style.display = "inline";
-    ans3.style.display = "inline";
-    correctAns.style.display = "inline";
-
-    if(numberOfAns == "4")
-    {
-    ans4.style.display = "inline";
-    }
-    if(numberOfAns == "5")
-    {
-    ans4.style.display = "inline";
-    ans5.style.display = "inline";
-    }
-    if(numberOfAns == "6")
-    {
-    ans4.style.display = "inline";
-    ans5.style.display = "inline";
-    ans6.style.display = "inline";
-    }
-    if(numberOfAns == "7")
-    {
-    ans4.style.display = "inline";
-    ans5.style.display = "inline";
-    ans6.style.display = "inline";
-    ans7.style.display = "inline";
-    }
-    if(numberOfAns == "8")
-    {
-    ans4.style.display = "inline";
-    ans5.style.display = "inline";
-    ans6.style.display = "inline";
-    ans7.style.display = "inline";
-    ans8.style.display = "inline";
-    }
-
-    if(numberOfCorrectAns == "2")
-    {
-    correctAns2.style.display = "inline";
-    }
-    if(numberOfCorrectAns == "3")
-    {
-    correctAns2.style.display = "inline";
-    correctAns3.style.display = "inline";
-    }
-    });
-
 
     addAqBTN.addEventListener('click', function() {
+
+        let tableOfAnswers = document.getElementById('dataTable');
+        let rowsOfTable = tableOfAnswers.rows;
+        let numberOfRowsInTable = rowsOfTable.length;
+        let rowIteratorIndex;
+
+        for(rowIteratorIndex=0; rowIteratorIndex<numberOfRowsInTable; rowIteratorIndex++)
+        {
+            let answer = rowsOfTable[rowIteratorIndex].cells[1].childNodes[0].value;
+            arrOfAnswers.push(answer);
+        }
+
     getRequestAttractions(funcToGetAttraction);
     });
 
@@ -185,6 +122,31 @@ function postRequestAmericanQuestion(aq,attr_id){
     window.location.href='/pick_aq_edit';
     }
 
+
+    function pushTds(iter){
+
+    let retArr = [];
+    let i=1;
+
+    if(what==1){
+        while(i<=iter){
+            retArr.push(document.getElementById("ans"+""+i).value);
+            i++;
+        }
+    }
+
+    if(what==2){
+        i=2;
+        retArr.push(document.getElementById("correctAns").value);
+        while(i<=iter){
+            retArr.push(document.getElementById("correctAns"+i).value);
+            i++;
+        }
+    }
+
+    return retArr;
+
+    }
 
     function fillWithText(what,iter){
 
