bbfa33ad34a78659d0f9ee1de54fc4b20c54935c
yonisGit <yoniel@post.bgu.ac.il>
2019-05-18 20:22:11 +0300

    דשגכךףל

---
+++ ManagementSystem/static/prop_map.js
@@ -0,0 +1,112 @@
+//from django.conf import settings
+
+let curPosClicked;
+let curMarker;
+let coordinates_of_last_click;
+// let points= [
+//     {lat: 31.263465932844372, lng: 34.801946282386783},
+//     {lat: 31.263065932844372, lng: 34.801146282386783},
+//     {lat: 31.263865932844372, lng: 34.802146282386783},
+//     {lat: 31.262773527283052, lng: 34.802075028419495}
+// ];
+
+
+window.onload=function () {
+
+
+};
+
+function initMapAndAttractionsprop(){
+        //alert("dasmaps");
+    initMap();
+    initAttractionsMarkers();
+}
+
+function initMap() {
+     map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: {lat: 31.262860, lng: 34.801753}
+    });
+    initAttractionsMarkers();
+    listenerForMap(map);
+    initPoints();
+    // alert("the number of points is now :" + points.length)
+    // var shortPath1 = JSON.parse(localStorage.getItem("short_path"));
+    //     var medPath1 = JSON.parse(localStorage.getItem("medium_path"));
+    //     var longPath1 = JSON.parse(localStorage.getItem("long_path"));
+          // alert("short:"+shortPath1.length +"\nmedium: "+medPath1.length +"\nlong: "+longPath1.length);
+
+
+}
+
+
+  function addEditListener(m) {
+      m.addListener('click', function() {
+
+
+          prev_m=m;
+           // markSpecificAttraction(pos);
+          // alert("the point "+m.position);
+  });
+  }
+
+function listenerForMap(map){
+    google.maps.event.addListener(map, 'click', (function(event) {
+        coordinates_of_last_click=event.latLng;
+        // alert(coordinates_of_last_click.lat()+","+coordinates_of_last_click.lng())
+        // alert(coordinates_of_last_click);
+        if(curPosClicked) {
+
+            curMarker.setMap(null);
+        }
+
+        curPosClicked = positionInMap(event.latLng.lat(), event.latLng.lng());
+        curMarker = markAttraction(curPosClicked);
+    }));
+}
+
+
+function initAttractionsMarkers() {
+    getRequestAttractions(markAttractions);
+}
+function getRequestAttractions(funcOnAttractions){
+    // the server port and my ip
+    serverRequest("GET", funcOnAttractions, 'http://'+ip+':12344/managementsystem/attraction/?format=json');
+    //alert("need to remove this alert and fix funcToGetAttraction()!");
+}
+
+function markAttractions(attractionsJSON){
+    //alert(window.innerHeight + " "+ window.innerWidth);
+    attractionsJSON.forEach(function (attr) {
+
+        let pos = {lat: attr['x'], lng: attr['y']};
+        marker_arr.push(pos);
+        localStorage.setItem("title"+pos,"attraction ID: "+attr['id']+"\nattraction name: "+attr['name']+"\nposition: ("+attr['x']+","+attr['y']+")");
+        markAttraction(pos);
+        // var currPoints = JSON.parse(localStorage.getItem("points"));
+        // currPoints.push(x.position);
+        // localStorage.setItem("points",JSON.stringify(currPoints));
+
+
+    });
+}
+
+
+function markAttraction(pos){
+        let marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          icon: {
+                url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                }
+        });
+        // marker_arr.push(marker);
+        marker.setMap(map);
+        addEditListener(marker);
+        return marker;
+}
+
+function positionInMap(lat, lng){
+          return {lat: lat, lng: lng};
+      }
+
