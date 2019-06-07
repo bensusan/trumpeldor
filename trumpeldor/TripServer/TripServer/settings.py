d1b0649dad4b1031a9eb57ad9454c1269f45ceaa
yonisGit <yoniel@post.bgu.ac.il>
2019-06-07 16:33:34 +0300

    lkjlk

--- trumpeldor/TripServer/TripServer/settings.py
+++ trumpeldor/TripServer/TripServer/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'TripServer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'PigiProject',
+        'NAME': 'ForAddInfo',
         'USER': 'postgres',
         'PASSWORD': 'trumpeldor',
         'HOST': '127.0.0.1',
