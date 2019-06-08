f956341e8e8274b64617beeeee9cf071b8e402b4
yonisGit <yoniel@post.bgu.ac.il>
2019-06-09 02:09:00 +0300

    lsdka

--- ManagementSystem/static/edit_feedbacks.js
+++ ManagementSystem/static/edit_feedbacks.js
@@ -13,19 +13,32 @@ function initializeBtnsFunctionality() {
     let deletecb = document.getElementById("delete_cb");
     let do_edit = document.getElementById("edit_chosen_fb");
     let do_delete = document.getElementById("delete_chosen_fb");
+    let edit_txt = document.getElementById("fb_edit_txt");
+
 
     editBTN.addEventListener('click', function () {
 
         editcb.style.display = "";
         do_edit.style.display = "";
+        edit_txt.style.display = "";
+        deletecb.style.display = "none";
+        do_delete.style.display = "none";
         do_edit.addEventListener('click', function () {
-            localStorage.setItem("attr_id_for_aq_edit", attractionObjToUseInHintDelete['id']);
-            window.location.href = '/edit_aq';
+            let fb_id = editcb.options[editcb.selectedIndex].value;
+            let type = localStorage.getItem("fbtype" + fb_id);
+            let fb = {
+                question:document.getElementById('fb_edit_txt').value,
+                kind: type
+            }
+            editRequestFeedback(fb, fb_id);
+            window.location.href = '/edit_feedbacks';
         });
     });
 
     deleteBTN.addEventListener('click', function () {
-
+        editcb.style.display = "none";
+        do_edit.style.display = "none";
+        edit_txt.style.display = "none";
         deletecb.style.display = "";
         do_delete.style.display = "";
         do_delete.addEventListener('click', function () {
@@ -36,13 +49,13 @@ function initializeBtnsFunctionality() {
     });
 }
 
-
 function funcForExistingFeedbacks(feedbacksJSON) {
     str = "";
     let i = 1;
     feedbacksJSON.forEach(function (fb) {
         let d_opt_id = "dcb" + i;
         let e_opt_id = "ecb" + i;
+        localStorage.setItem("fbtype" + fb['id'], fb['kind']);
         document.getElementById(d_opt_id).innerText = fb['question']
         document.getElementById(d_opt_id).value = fb['id'];
         document.getElementById(d_opt_id).style.display = 'inline';
