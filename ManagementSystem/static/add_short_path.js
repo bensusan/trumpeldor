c1afcd2c6d741cd72dced2c87b74f8bd86abcc4d
yonisGit <yoniel@post.bgu.ac.il>
2019-06-15 23:24:42 +0300

    sda

--- ManagementSystem/static/add_short_path.js
+++ ManagementSystem/static/add_short_path.js
@@ -53,7 +53,7 @@ function functionToGetNames(attractionsJSON) {
         if ((p.lat).toFixed(8) == lat && (p.lng).toFixed(8) == lng) {
             str_of_points = str_of_points + attr['name'] + "<br />";
             var border = document.getElementById("border_of_points");
-            border.style.display = "none";
+            border.style.display = "";
             document.getElementById("showing_added_points").innerHTML = str_of_points;
             document.getElementById("showing_added_points").style.fontWeight = 'bold';
         }
