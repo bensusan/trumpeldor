fad81c0edcfc7426180a1ce16e4fb4d0fe9d51a9
yonisGit <yoniel@post.bgu.ac.il>
2019-06-15 22:06:29 +0300

    sda

--- ManagementSystem/static/pick_hint_edit.js
+++ ManagementSystem/static/pick_hint_edit.js
@@ -1,120 +1,106 @@
-var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
+var loadFile = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
 };
 
 var str;
 var attractionObjToUseInHintDelete;
 
-function funcForExistingHints(attractionsJSON){
-        let name = localStorage.getItem("name_for_add_aq");
-        let desc = localStorage.getItem("desc_for_add_aq");
-      // alert("in get name! "+"of the origin : " + lat + " , " + lng);
-        attractionsJSON.forEach(function (attr) {
-        let p = {name: attr['name'], description:attr['description']};
-        if(p.name===name && p.description===desc)
-        {
-            getRequestHints(hints_func,attr['id']);
-            attractionObjToUseInHintDelete=attr;
+function funcForExistingHints(attractionsJSON) {
+    let name = localStorage.getItem("name_for_add_aq");
+    let desc = localStorage.getItem("desc_for_add_aq");
+    // alert("in get name! "+"of the origin : " + lat + " , " + lng);
+    attractionsJSON.forEach(function (attr) {
+        let p = {name: attr['name'], description: attr['description']};
+        if (p.name === name && p.description === desc) {
+            getRequestHints(hints_func, attr['id']);
+            attractionObjToUseInHintDelete = attr;
         }
-        });
+    });
 
 }
 
 function hints_func(hintsJSON) {
-        str="";
-        let i=1;
-        let imageCounter = 1;
-        let videoCounter = 1;
+    str = "";
+    let i = 1;
+    let imageCounter = 1;
+    let videoCounter = 1;
 
-        hintsJSON.forEach(function (hint) {
-            let e_opt_id = "ecb"+i;
-            let d_opt_id = "dcb"+i;
+    hintsJSON.forEach(function (hint) {
+        let e_opt_id = "ecb" + i;
+        let d_opt_id = "dcb" + i;
 
-            if(hint['data'].substring(0,16) == 'data:application')
-            {
-                document.getElementById(d_opt_id).innerText = "media"+imageCounter;
-                document.getElementById(e_opt_id).innerText = "media"+imageCounter;
-            }
-            else
-            {
-                 if(hint['data'].substring(0,10) == 'data:video')
-            {
-                document.getElementById(d_opt_id).innerText = "VideoMedia"+videoCounter;
-                document.getElementById(e_opt_id).innerText = "VideoMedia"+videoCounter;
-            }
-            else
-            {
+        if (hint['data'].substring(0, 16) == 'data:application') {
+            document.getElementById(d_opt_id).innerText = "media" + imageCounter;
+            document.getElementById(e_opt_id).innerText = "media" + imageCounter;
+        } else {
+            if (hint['data'].substring(0, 10) == 'data:video') {
+                document.getElementById(d_opt_id).innerText = "VideoMedia" + videoCounter;
+                document.getElementById(e_opt_id).innerText = "VideoMedia" + videoCounter;
+            } else {
                 document.getElementById(d_opt_id).innerText = hint['data'];
                 document.getElementById(e_opt_id).innerText = hint['data'];
             }
-            }
+        }
 
-
-
-            document.getElementById(e_opt_id).value = hint['id'];
-            document.getElementById(e_opt_id).style.display='inline';
-            document.getElementById(d_opt_id).value = hint['id'];
-            document.getElementById(d_opt_id).style.display='inline';
-            i=i+1;
-            //data:application
-            let dataOfHint = hint['data'];
-            if(dataOfHint.substring(0,16) == 'data:application')
-                str=str+" data: "+ "media"+imageCounter+"<br />";
+        document.getElementById(e_opt_id).value = hint['id'];
+        document.getElementById(e_opt_id).style.display = '';
+        document.getElementById(d_opt_id).value = hint['id'];
+        document.getElementById(d_opt_id).style.display = '';
+        i = i + 1;
+        //data:application
+        let dataOfHint = hint['data'];
+        if (dataOfHint.substring(0, 16) == 'data:application')
+            str = str + " data: " + "media" + imageCounter + "<br />";
+        else {
+            if (hint['data'].substring(0, 10) == 'data:video')
+                str = str + " data: " + "VideoMedia" + videoCounter + "<br />";
             else
-            {
-              if(hint['data'].substring(0,10) == 'data:video')
-                str=str+" data: "+ "VideoMedia"+videoCounter+"<br />";
-              else
                 str=str+" text: "+ hint['data'].split(';;')[0]+"<br />";
             }
 
-
-            // alert(str);
-
-            var output = document.getElementById("result");
-            var outputVideo = document.getElementById("resultVideos");
-
-            if(hint['data'].substring(0,16) == 'data:application') {
-                imageCounter++;
-                var img = document.createElement("img");
-                img.src = dataOfHint;
-                img.className = 'thumbnail';
-                var div = document.createElement("div");
-                div.appendChild(img);
+        var output = document.getElementById("result");
+        var outputVideo = document.getElementById("resultVideos");
 
 
-                  output.insertBefore(div,null);
-            }
+        if (hint['data'].substring(0, 16) == 'data:application') {
+            imageCounter++;
+            var img = document.createElement("img");
+            img.src = dataOfHint;
+            img.className = 'thumbnail';
+            var div = document.createElement("div");
+            div.appendChild(img);
+            output.insertBefore(div, null);
+        }
+
+        if (hint['data'].substring(0, 10) == 'data:video') {
+            videoCounter++;
+            var vid = document.createElement("video");
+            vid.src = dataOfHint;
+            vid.className = 'thumbnail';
+            var div = document.createElement("div");
+            vid.autoplay = true;
+            div.appendChild(vid);
+
+            outputVideo.insertBefore(div, null);
+        }
 
 
-            if(hint['data'].substring(0,10) == 'data:video')
-            {
-                videoCounter++;
-                var vid = document.createElement("video");
-                vid.src = dataOfHint;
-                vid.className = 'thumbnail';
-                var div = document.createElement("div");
-                vid.autoplay = true;
-                div.appendChild(vid);
+    });
 
-                outputVideo.insertBefore(div,null);
-            }
-
-        });
-        document.getElementById("existing_hints").innerHTML = str ;
-        document.getElementById("existing_hints").style.fontWeight = 'bold';
-        document.getElementById("existing_hints").style.fontFamily='david';
-        document.getElementById("existing_hints").style.fontSize='24px';
+    document.getElementById("existing_hints").innerHTML = str;
+    document.getElementById("existing_hints").style.fontWeight = 'bold';
+    document.getElementById("existing_hints").style.fontFamily = 'david';
+    document.getElementById("existing_hints").style.fontSize = '24px';
 }
 
 window.onload = function () {
     let editMenuBTN = document.getElementById('editHintBTNmenu');
-    editMenuBTN.addEventListener('click', function(){
+    editMenuBTN.addEventListener('click', function () {
         wantToEditButton();
     });
     let deleteMenuBTN = document.getElementById('deleteHintBTNmenu');
-    deleteMenuBTN.addEventListener('click', function(){
+    deleteMenuBTN.addEventListener('click', function () {
         wantToDeleteButton();
     });
     getRequestAttractions(funcForExistingHints);
@@ -123,135 +109,135 @@ window.onload = function () {
 };
 
 
-function wantToEditButton(){
+function wantToEditButton() {
     var delTitle = document.getElementById("deleteTitle");
-            delTitle.style.display = "none";
+    delTitle.style.display = "none";
 
-            var deleteChosenHintBTN = document.getElementById("delete_chosen_hint");
-            deleteChosenHintBTN.style.display = "none";
+    var deleteChosenHintBTN = document.getElementById("delete_chosen_hint");
+    deleteChosenHintBTN.style.display = "none";
 
-            var comboDelete = document.getElementById("delete_cb");
-            comboDelete.style.display = "none";
+    var comboDelete = document.getElementById("delete_cb");
+    comboDelete.style.display = "none";
 
-      var edTitle = document.getElementById("editTitle");
-            edTitle.style.display = "inline";
+    var edTitle = document.getElementById("editTitle");
+    edTitle.style.display = "inline";
 
-            var comboEdit = document.getElementById("edit_cb");
-            comboEdit.style.display = "inline";
+    var comboEdit = document.getElementById("edit_cb");
+    comboEdit.style.display = "inline";
 
-            var editChosenHintBTN = document.getElementById("edit_chosen_hint");
-            editChosenHintBTN.style.display = "inline";
+    var editChosenHintBTN = document.getElementById("edit_chosen_hint");
+    editChosenHintBTN.style.display = "inline";
 
-            window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight);
 
-            editChosenHintBTN.addEventListener('click', function() {
-                let chosen_hint_id = comboEdit.options[comboEdit.selectedIndex].value;
-                // let hint_id_that_was_picked = document.getElementById("write_hint_id_to_edit").value;
-                localStorage.setItem("hint_id_to_edit", chosen_hint_id);
-                localStorage.setItem("attr_id_for_hint_edit", attractionObjToUseInHintDelete['id']);
-                window.location.href='/edit_hint_edit';
-            });
+    editChosenHintBTN.addEventListener('click', function () {
+        let chosen_hint_id = comboEdit.options[comboEdit.selectedIndex].value;
+        // let hint_id_that_was_picked = document.getElementById("write_hint_id_to_edit").value;
+        localStorage.setItem("hint_id_to_edit", chosen_hint_id);
+        localStorage.setItem("attr_id_for_hint_edit", attractionObjToUseInHintDelete['id']);
+        window.location.href = '/edit_hint_edit';
+    });
 }
 
 
-function wantToDeleteButton(){
+function wantToDeleteButton() {
 
-            var editChosenHintBTN = document.getElementById("edit_chosen_hint");
-            editChosenHintBTN.style.display = "none";
+    var editChosenHintBTN = document.getElementById("edit_chosen_hint");
+    editChosenHintBTN.style.display = "none";
 
-            var comboEdit = document.getElementById("edit_cb");
-            comboEdit.style.display = "none";
+    var comboEdit = document.getElementById("edit_cb");
+    comboEdit.style.display = "none";
 
-            var delTitle = document.getElementById("deleteTitle");
-            delTitle.style.display = "inline";
+    var delTitle = document.getElementById("deleteTitle");
+    delTitle.style.display = "inline";
 
-        var edTitle = document.getElementById("editTitle");
-            edTitle.style.display = "none";
+    var edTitle = document.getElementById("editTitle");
+    edTitle.style.display = "none";
 
-            var comboDelete = document.getElementById("delete_cb");
-            comboDelete.style.display = "inline";
+    var comboDelete = document.getElementById("delete_cb");
+    comboDelete.style.display = "inline";
 
-            var deleteChosenHintBTN = document.getElementById("delete_chosen_hint");
-            deleteChosenHintBTN.style.display = "inline";
+    var deleteChosenHintBTN = document.getElementById("delete_chosen_hint");
+    deleteChosenHintBTN.style.display = "inline";
 
-                        window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight);
 
-            deleteChosenHintBTN.addEventListener('click', function() {
-                getRequestHints(funcInOrderToDeleteHint,attractionObjToUseInHintDelete['id']);
-            });
+    deleteChosenHintBTN.addEventListener('click', function () {
+        getRequestHints(funcInOrderToDeleteHint, attractionObjToUseInHintDelete['id']);
+    });
 }
 
 function funcInOrderToDeleteHint(hintsJSON) {
     var comboDelete = document.getElementById("delete_cb");
     let hint_id_that_was_picked = comboDelete.options[comboDelete.selectedIndex].value;
-   // let number_hint_id = Number(hint_id_that_was_picked);
-      hintsJSON.forEach(function (hint) {
-          // alert("the id is: "+attr['id']);
+    // let number_hint_id = Number(hint_id_that_was_picked);
+    hintsJSON.forEach(function (hint) {
+        // alert("the id is: "+attr['id']);
         // alert("in get name! "+"of the origin : " + lat + " , " + lng + "\n of the other: "+p.lat +" , "+ p.lng);
-        if(hint['id']==hint_id_that_was_picked)
-        {
+        if (hint['id'] == hint_id_that_was_picked) {
             //alert("before delete hint!");
-            deleteRequestHint(attractionObjToUseInHintDelete['id'],hint['id']);
-            window.location.href='/pick_hint_edit';
+            deleteRequestHint(attractionObjToUseInHintDelete['id'], hint['id']);
+            window.location.href = '/pick_hint_edit';
         }
-      });
+    });
 
 }
 
 
-
 function localFileVideoPlayer() {
-	'use strict';
-  let URL = window.URL || window.webkitURL;
-  let displayMessage = function (message, isError) {
-    let element = document.querySelector('#message');
-    element.innerHTML = message;
-    element.className = isError ? 'error' : 'info'
-  };
-  let playSelectedFile = function (event) {
-    let file = this.files[0];
-    let type = file.type;
-    let videoNode = document.querySelector('video');
-    let canPlay = videoNode.canPlayType(type);
-    if (canPlay === '') canPlay = 'no';
-    let message = 'Can play type "' + type + '": ' + canPlay;
-    let isError = canPlay === 'no';
-    //displayMessage(message, isError)
+    'use strict';
+    let URL = window.URL || window.webkitURL;
+    let displayMessage = function (message, isError) {
+        let element = document.querySelector('#message');
+        element.innerHTML = message;
+        element.className = isError ? 'error' : 'info'
+    };
+    let playSelectedFile = function (event) {
+        let file = this.files[0];
+        let type = file.type;
+        let videoNode = document.querySelector('video');
+        let canPlay = videoNode.canPlayType(type);
+        if (canPlay === '') canPlay = 'no';
+        let message = 'Can play type "' + type + '": ' + canPlay;
+        let isError = canPlay === 'no';
+        //displayMessage(message, isError)
 
-    if (isError) {
-      return
-    }
+        if (isError) {
+            return
+        }
 
-    let fileURL = URL.createObjectURL(file);
-    videoNode.src = fileURL;
+        let fileURL = URL.createObjectURL(file);
+        videoNode.src = fileURL;
 
-  };
-  let inputNode = document.querySelector('input');
-  inputNode.addEventListener('change', playSelectedFile, false);
+    };
+    let inputNode = document.querySelector('input');
+    inputNode.addEventListener('change', playSelectedFile, false);
 }
 
 
 function donePickingHints() {
-    window.location.href='/attractions';
+    window.location.href = '/attractions';
 }
 
 
-function getRequestHints(funcOnHints,attr_id){
+function getRequestHints(funcOnHints, attr_id) {
     // serverRequest("GET", funcOnAttractions, 'http://192.168.1.12:12344/managementsystem/attraction/?format=json');
     // the server port and my ip
-    serverRequest("GET", funcOnHints, 'http://'+ip+':12344/managementsystem/attraction/'+ attr_id+
+    serverRequest("GET", funcOnHints, 'http://' + ip + ':12344/managementsystem/attraction/' + attr_id +
         '/hint/?format=json');
     //alert("need to remove this alert and fix funcToGetAttraction()!");
 }
 
 
-function postRequestHint(the_hint,attr_id){
- //   alert("hint blat");
-    serverRequest("POST", function noop(dummy){}, 'http://'+ip+':12344/managementsystem/attraction/'+
-        attr_id+'/hint/',
+function postRequestHint(the_hint, attr_id) {
+    //   alert("hint blat");
+    serverRequest("POST", function noop(dummy) {
+        }, 'http://' + ip + ':12344/managementsystem/attraction/' +
+        attr_id + '/hint/',
         JSON.stringify(the_hint));
 }
 
-function deleteRequestHint(attr_id,hint_id){
-     serverRequest("DELETE", function noop(dummy){}, 'http://'+ip+':12344/managementsystem/attraction/'+attr_id+'/hint/'+hint_id+'/');
-    }
+function deleteRequestHint(attr_id, hint_id) {
+    serverRequest("DELETE", function noop(dummy) {
+    }, 'http://' + ip + ':12344/managementsystem/attraction/' + attr_id + '/hint/' + hint_id + '/');
+}
